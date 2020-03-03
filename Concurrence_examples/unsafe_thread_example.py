28e68f4b30cd0a307eec030165c6a48f0148beb5
Mia von Steinkirch <mia.steinkirch@gmail.com>
2020-03-02 18:35:15 -0800

    add thread example

---
+++ Concurrence_examples/unsafe_thread_example.py
@@ -0,0 +1,20 @@
+import time
+import threading
+
+counter = 0
+threads = []
+
+def count():
+    global counter
+    for _ in range(100):
+        counter += 1
+
+for _ in range(100):
+    thread = threading.Thread(target=count)
+    thread.start()
+    threads.append(thread)
+ 
+for thread in threads:
+    thread.join()
+
+print(f"Count: {counter}")
