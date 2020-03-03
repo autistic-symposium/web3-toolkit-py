e686184be18b0b17dac24df6ca0cb54f76b8bc63
Mia von Steinkirch <mia.steinkirch@gmail.com>
2020-03-02 18:42:49 -0800

    add safe thread example

---
+++ Concurrence_examples/safe_thread_example.py
@@ -0,0 +1,24 @@
+import threading
+
+counter = 0
+threads = []
+
+lock = threading.Lock()
+
+
+def count_with_lock():
+
+    global counter
+
+    for _ in range(100):
+        with lock:
+            counter += 1
+
+
+for _ in range(100):
+    thread = threading.Thread(target=count_with_lock)
+    thread.start()
+    threads.append(thread)
+
+
+print(counter)
